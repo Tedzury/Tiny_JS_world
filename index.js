f983e2b8b4da728f4417dee099373059a368355d
Tedzury <asdrogachev@gmail.com>
2022-08-23 10:36:06 +0300

    Refactored code. V2.0

--- index.js
+++ index.js
@@ -7,58 +7,57 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-catsPhrase = 'meow-meow skinbag';
+catsPhrase = "meow-meow skinbag";
 
 const tinyWorldInhabitants = [
-   {
-      species: 'dog',
-      name: 'Barbos',
-      gender: 'male',
-      legs: 4,
-      hands: 0,
-      saying: 'gav-gav kaje pes',
-      friends: ['definitely everybody in the world !']
-   },
-   {
-      species: 'cat',
-      name: 'Sonya',
-      gender: 'female',
-      legs: 4,
-      hands: 0,
-      saying : catsPhrase,
-      friends: ['definitely nobody except her own slaves']
-   },
-   {
-      species: 'human',
-      name: 'Oleksii',
-      gender: 'male',
-      legs: 2,
-      hands: 2,
-      saying: 'Wanna be frontend ninja in future!',
-      friends: ['Alex', 'Victoria', 'every lovely kottan on the course']
-   },
-   {
-      species: 'human',
-      name: 'Victoria',
-      gender: 'female',
-      legs: 2,
-      hands: 2,
-      saying: 'When will you bring the money to home, honey? ',
-      friends: ['Natasha', 'Katya', 'Lada']
-   },
-   {
-      species: 'catWoman',
-      name: 'Anjela',
-      gender: 'female',
-      legs: 2,
-      hands: 2,
-      saying : catsPhrase,
-      friends: ['definitely nobody except her own slaves, she\'s a cat, you know...']
-   }
-
-]
-
-
+  {
+    species: "dog",
+    name: "Barbos",
+    gender: "male",
+    legs: 4,
+    hands: 0,
+    saying: "gav-gav kaje pes",
+    friends: ["definitely everybody in the world !"],
+  },
+  {
+    species: "cat",
+    name: "Sonya",
+    gender: "female",
+    legs: 4,
+    hands: 0,
+    saying: catsPhrase,
+    friends: ["definitely nobody except her own slaves"],
+  },
+  {
+    species: "human",
+    name: "Oleksii",
+    gender: "male",
+    legs: 2,
+    hands: 2,
+    saying: "Wanna be frontend ninja in future!",
+    friends: ["Alex", "Victoria", "every lovely kottan on the course"],
+  },
+  {
+    species: "human",
+    name: "Victoria",
+    gender: "female",
+    legs: 2,
+    hands: 2,
+    saying: "When will you bring the money to home, honey?",
+    friends: ["Natasha", "Katya", "Lada"],
+  },
+  {
+    species: "catWoman",
+    name: "Anjela",
+    gender: "female",
+    legs: 2,
+    hands: 2,
+    saying: catsPhrase,
+    friends: [
+      "definitely nobody except her own slaves, she's a cat, you know...",
+    ],
+  },
+];
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -78,23 +77,24 @@ const tinyWorldInhabitants = [
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
+   const keys = [
+      'species',
+      'name',
+      'gender',
+      'legs',
+      'hands',
+      'saying',
+      'friends',
+    ];
 
-   function formOutput(obj) {
-
-      const outputArr = [obj.species, obj.name, obj.gender, obj.legs, obj.hands, obj.saying, obj.friends];
-
-      for (let i = 0; i < outputArr.length; i++) {
-         if (Array.isArray(outputArr[i])) {
-            outputArr[i] = outputArr[i].join(', ')
+tinyWorldInhabitants.forEach((person) => {
+   print(
+      keys.map((key) => {
+         if (Array.isArray(person[key])) {
+           person[key] = person[key].join(', ')
          }
-      }
-
-      const result = outputArr.join('; ');
-      return result
-   }
-   
-   tinyWorldInhabitants.forEach( item => print(formOutput(item)));
-   
-
-
-
+        return person[key]
+      })
+      .join('; ')
+   )
+})
